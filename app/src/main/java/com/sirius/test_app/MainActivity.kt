265305b55a54989a82e962397c4f39d055a58c9c
iktkhor <txor2009@mail.ru>
2022-07-15 10:51:05 +0300

    first

--- app/src/main/java/com/sirius/test_app/MainActivity.kt
+++ app/src/main/java/com/sirius/test_app/MainActivity.kt
@@ -2,10 +2,17 @@ package com.sirius.test_app
 
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
+import android.widget.Button
 
 class MainActivity : AppCompatActivity() {
+    private lateinit var installButton: Button
+
+
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_main)
+
+        installButton = findViewById(R.id.install_button)
+
     }
 }
